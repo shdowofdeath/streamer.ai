bcda21055de06d266eb1b959e64add06704074ee
Elhay Efrat <elhayefrat@gmail.com>
2021-05-24 22:26:10 +0300

    Update straemmer.py

--- straemmer.py
+++ straemmer.py
@@ -1,2 +1,13 @@
-import streamlit as st 
-st.write('walla')
+import streamlit as st
+import numpy as np
+from PIL import Image
+
+st.title('הפרוייקט של AI')
+st.balloons()
+# st.write("בבקשה להעלות תמונה  ")
+
+img_file_buffer = st.file_uploader("בבקשה להעלות תמונה  ")
+if img_file_buffer is not None:
+    image = Image.open(img_file_buffer)
+    img_array = np.array(image) # if you want to pass it to OpenCV
+    st.image(image, caption="The caption", use_column_width=True)
