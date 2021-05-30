e908efee00169f724325b4af91ee4b566da6bc8a
Elhay Efrat <elhayefrat@gmail.com>
2021-05-30 15:12:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,26 +2,18 @@ import streamlit as st
 import numpy as np
 from PIL import Image
 import cv2 
+import tensorflow as tf
 
 st.title('Storganizer')
 st.balloons()
 # st.write("בבקשה להעלות תמונה  ")
 
-def averagecolor(image):
-    return np.mean(image, axis=(0, 1))
+
 
 
 def predict(image):
-    img = cv2.imread(image)
-    img_features = averagecolor(img)
-    calculated_distances = []
-    for card in (trainX2):
-        calculated_distances.append(np.linalg.norm(img_features-card))
-    prediction =  trainY2[np.argmin(calculated_distances)]
-    
-    print (filename + ": " + prediction)
-    filenames.append(filename)
-    predictedY.append(prediction)
+    reconstructed_model = keras.models.load_model("model")
+    model.predict(test_input), reconstructed_model.predict(test_input)
 
 img_file_buffer = st.file_uploader("בבקשה להעלות תמונה  ")
 if img_file_buffer is not None:
